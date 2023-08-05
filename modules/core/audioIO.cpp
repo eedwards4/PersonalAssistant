94749d1d2597af54874707a31730b72886cdbdce
Ethan Edwards <evanevansonbusiness@gmail.com>
2023-08-05 12:06:41 -0700

    Starting to build audio wrapper

--- modules/core/audioIO.cpp
+++ modules/core/audioIO.cpp
@@ -9,8 +9,70 @@ audioIO::audioIO(int listen_length) {
     running = false;
 }
 
-bool audioIO::listen() {
-    return false;
+audioIO::~audioIO() {
+    if(mic){
+        SDL_CloseAudioDevice(mic);
+    }
+    running = false;
 }
 
+bool audioIO::startListen(int sample_rate) {
+    SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
+    int mics = SDL_GetNumAudioDevices(SDL_TRUE);
+    char input;
 
+    if (SDL_Init(SDL_INIT_AUDIO) < 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL FAILURE: %s\n", SDL_GetError());
+        return false;
+    }
+
+    SDL_SetHintWithPriority(SDL_HINT_AUDIO_RESAMPLING_MODE, "medium", SDL_HINT_OVERRIDE);
+
+    // Init microphone
+
+    SDL_AudioSpec mic_requested;
+    SDL_AudioSpec mic_obtained;
+
+    SDL_zero(mic_requested);
+    SDL_zero(mic_obtained);
+
+    mic_requested.freq = p.sample_rate;
+    mic_requested.format = AUDIO_F32;
+    mic_requested.channels = 1;
+    mic_requested.samples = p.sample_request;
+
+    cout << "Found " << mics << " microphones. Please select:" << endl;
+    for (int i = 0; i < mics;){
+        cout << i << ":" << SDL_GetAudioDeviceName(i, SDL_TRUE) << endl;
+        cout << "Select? (y/n): ";
+        cin >> input;
+        if (input == 'y'){
+            cout << "Initializing microphone..." << endl;
+            mic = SDL_OpenAudioDevice(SDL_GetAudioDeviceName(i, SDL_TRUE), SDL_TRUE, &mic_requested, &mic_obtained, 0);
+            if (!mic){
+                cout << "MIC INIT FAILURE: " << SDL_GetError() << endl;
+                return false;
+            }
+            cout << "Microphone initialized." << endl;
+            break;
+        } else if (input == 'n'){
+            i++;
+        } else {
+            cout << "Invalid input. Please try again." << endl;
+            i--;
+        }
+    }
+
+    sampleRate = mic_obtained.freq;
+    audio.resize((sampleRate*recordLen)/1000);
+
+    return true;
+}
+
+void audioIO::SDLCallback(uint8_t *stream, int len) {
+    if (!running){
+        return;
+    }
+
+
+}
--- modules/core/audioIO.h
+++ modules/core/audioIO.h
@@ -6,21 +6,36 @@
 #define PERSONALASSISTANT_AUDIOIO_H
 
 #include <SDL_audio.h>
+#include <iostream>
 #include <atomic>
+#include <vector>
+#include <string>
 #include <SDL.h>
 
 using namespace std;
 
+struct params{
+    int sample_rate = 0;
+    int sample_request = 1024;
+
+};
+
 class audioIO { // I'm in your walls
 public:
+    // Construction
     audioIO(int listen_length);
-    ~audioIO() = default;
+    ~audioIO();
+    void SDLCallback(uint8_t* stream, int len);
 
-    bool listen();
+    bool startListen(int sample_rate);
     bool pauseListen();
 private:
+    params p;
+    SDL_AudioDeviceID mic = 0;
     int recordLen = 0;
     int sampleRate = 0;
+
+    vector<float> audio;
 
     atomic_bool running{};
 };
